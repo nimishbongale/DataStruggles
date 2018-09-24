f17d33bb95e49589210a38ac17f9de777bb2cd19
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-09-24 05:12:53 +0000

    Add files via upload

---
+++ fibrec.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class fibrec
+{
+	static int fib(int n)
+	{
+		if(n==1)
+		return 0;
+		if(n==2)
+		return 1;		
+		return (fib(n-1)+fib(n-2));
+	}
+		
+	public static void main(String args[])
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of terms \t");
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		System.out.println(fib(i)+"\t");
+	}
+		
+}
+
