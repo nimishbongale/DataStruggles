80735eb663591bd8c80ae361e7dd28d18b24b6f8
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-10-27 20:38:01 +0000

    Add files via upload

---
+++ binsearchrec.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int m;
+
+void binsearch(int a[],int l,int u,int ele)
+{
+	m=(l+u)/2;
+	if(u==(l+1))
+	{
+		printf("Element not found!");
+		return;
+	}
+	if(a[m]==ele)
+	{
+		printf("Element found at pos %d",(m+1));
+		return;
+	}
+	else if(ele>a[m])
+	binsearch(a,m,u,ele);
+	else
+	binsearch(a,l,m,ele);
+}
+
+int main()
+{
+	int n,num,i;
+	printf("Enter the no. of elements: ");
+	scanf("%d",&num);
+	int b[num];
+	printf("Enter a sorted array\n");
+	for(i=0;i<num;i++)
+	scanf("%d",&b[i]);
+	printf("Enter the element to be searched for: ");
+	scanf("%d",&n);
+	binsearch(b,0,num+1,n);
+	return 0;
+}
