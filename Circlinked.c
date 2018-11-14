09300cdcb60cc884a2cc29d7bfe01194e556e196
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-11-14 18:48:22 +0000

    Add files via upload

---
+++ Circlinked.c
@@ -0,0 +1,199 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *link;
+}*front=NULL,*head=NULL,*tail=NULL,*ptr,*p;
+
+struct node* createNode()
+{
+	struct node *newNode=(struct node*)malloc(sizeof(struct node));
+	if(newNode==NULL)
+	{
+	printf("Heap full!");
+	exit(0);
+	}
+	else
+	{
+		printf("Enter the data\n");
+		scanf("%d",&(newNode->data));
+		newNode->link=NULL;
+	}
+	return newNode;
+}
+
+void InsertBegin()
+{
+	ptr=createNode();
+	if(head==NULL)
+	{
+		head=tail=ptr;
+		head->link=head;
+		tail=head;
+	}
+	else
+	{
+	ptr->link=head;
+	head=ptr;
+	tail->link=head;
+	}
+}
+
+void InsertEnd()
+{
+	if(head==NULL)
+	InsertBegin();
+	ptr=createNode();
+	tail->link=ptr;
+	ptr->link=head;
+	tail=ptr;
+}
+
+void InsertMiddle(int ele)
+{
+	p=head;
+	while((p!=tail)&&(p->data!=ele))
+	p=p->link;
+	if(p==NULL)
+	printf("No node found!");
+	else if(p==head)
+	InsertBegin();
+	else if(p==tail)
+	InsertEnd();
+	else
+	{
+		ptr=createNode();
+	    ptr->link=p->link;
+	    p->link=ptr;
+	}
+}
+
+void DeleteBegin()
+{
+	if(head==NULL)
+	printf("Circular Queue empty!");
+	else if (head==tail)
+	{
+		free(head);
+		free(tail);
+		head=NULL;
+		tail=NULL;
+	}
+	else
+	{
+		p=head;
+		head=head->link;
+		tail->link=head;
+		free(p);
+	}
+}
+
+void DeleteEnd()
+{
+	if(head==NULL)
+	printf("Circular Queue empty!");
+	else if(head==tail)
+	{
+		free(head);
+		head=NULL;
+		tail=NULL;
+	}
+	else
+	{
+	for(p=head;p->link!=tail;p=p->link);
+	free(tail);
+	tail=p;
+	tail->link=head;
+	}
+}
+
+void DeleteMiddle(int ele)
+{
+	p=head;
+	if(head==NULL)
+	printf("Circular Queue is empty!");
+	else if(head->data==ele)
+	DeleteBegin();
+	else
+	{
+	while((p!=tail)&&((p->link)->data!=ele))
+	p=p->link;
+	if(p==NULL)
+	printf("No node found!");
+	else
+		{
+			ptr=p->link;
+			p->link=(p->link)->link;
+			free(ptr);
+		}
+	}
+}
+
+void display()
+{
+	p=head;
+	if(p!=NULL)
+	{
+	do
+	{
+		printf("%d->",p->data);
+		p=p->link;
+	}while(p!=head);
+	}
+}
+
+int main()
+{
+	int ch,d;
+	while(1)
+	{
+	printf("\n*****MENU*****\n1.Insert in the beginning\n2.Insert in Middle\n3.Insert in the End\n4.Delete in the Beginning\n5.Delete in the Middle\n6.Delete in the End\n7.Display the contents\nEnter anything else to exit: ");
+	scanf("%d",&ch);
+	
+	switch(ch)
+	{
+		case 1:
+			InsertBegin();
+			break;
+			
+			case 2:
+				printf("Enter the data of the node before which the new node is to be inserted\n");
+				scanf("%d",&d);
+				InsertMiddle(d);
+				break;
+				
+				case 3:
+					InsertEnd();
+					break;
+					
+					case 4:
+						DeleteBegin();
+						break;
+						
+						case 5:
+						printf("Enter the data of the node to be deleted\n");
+						scanf("%d",&d);
+						DeleteMiddle(d);
+						break;
+						
+						case 6:
+							DeleteEnd();
+							break;
+						
+						
+						case 7:
+							printf("\nCurrent list:\n");
+							display();
+							printf("\n");
+							break;
+								
+								default:
+								exit(0);
+							}
+							
+						}
+						return 0;
+					}
+
---
+++ DoubleLL.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *prev;
+    int number;
+    struct node *next;
+}*head=NULL,*newNode=NULL,*tail=NULL,*sptr=NULL,*p=NULL, *after=NULL, *before=NULL;
+
+struct node* createNewNode()
+{
+    int data;
+    newNode=(struct node*)malloc(sizeof(struct node));
+    if(newNode==NULL)
+    {
+    	printf("Heap Memory full!");
+    	exit(0);
+	}
+    newNode->prev=newNode->next=NULL;
+    printf("\nEnter the data : ");
+    scanf("%d", &(newNode->number));
+    return newNode;
+}
+
+struct node * mysearch(int data)
+{
+	for(p=head;p!=NULL;p=p->next)
+	if(p->number==data)
+	return p;
+    return NULL;
+}
+
+void insertBegin()
+{
+	newNode=createNewNode();
+    if (head == NULL)
+    {
+		head = newNode;
+        tail = head;
+    }
+    else
+    {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+}
+
+void insertEnd()
+{
+	if(tail==NULL)
+	insertBegin();
+    newNode=createNewNode();
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+void insertBefore(int ele)
+{
+	sptr=mysearch(ele);
+	if(sptr==NULL)
+	printf("No node found!");
+	else if(sptr==head)
+	insertBegin();
+	else
+   	{
+   		before=sptr->prev;
+        newNode=createNewNode();
+   		before->next=newNode;
+   		newNode->prev=before;
+    	newNode->next = sptr;
+    	sptr->prev=newNode;
+	}
+}
+
+void insertAfter(int ele)
+{
+	sptr=mysearch(ele);
+	if(sptr==NULL)
+	printf("No node found!");
+	else if(sptr == tail)
+    insertEnd();
+    else
+    {
+    	after=sptr->next;
+        newNode=createNewNode();
+        sptr->next = newNode;
+        newNode->prev = sptr;
+        newNode->next=after;
+        after->prev=newNode;
+    }
+    
+}
+
+void display()
+{
+    p=head;
+    printf("\nCurrent Double linked list:\n ");
+    for(p=head;p!=NULL;p=p->next)
+    printf("%d->", p->number);
+}
+
+void deleteBegin()
+{
+	if(head==NULL)
+	printf("Queue empty!");
+	else
+	{
+    head=head->next;
+    free(head->prev);
+    head->prev=NULL;
+	}
+}
+
+void deleteEnd()
+{
+	if(head==NULL)
+	printf("Queue empty!");
+	else
+	{
+	tail=tail->prev;
+	free(tail->next);
+	tail->next=NULL;
+	}
+}
+
+void deleteMid(int key)
+{
+	if(head==NULL)
+	printf("Queue empty!");
+	else 
+	{
+	sptr=mysearch(key);
+	if(sptr==NULL)
+	printf("No node found!");
+	else if(sptr==head)
+	deleteBegin();
+	else if(sptr==tail)
+	deleteEnd();
+	else
+	{
+		before=sptr->prev;
+		after=sptr->next;
+		before->next=after;
+		after->prev=before;
+		free(sptr);
+	}
+	}
+}
+
+int main()
+{
+    int ch,data;
+    while (1)
+    {
+    printf("\n 1 - Insert in the Beginning");
+    printf("\n 2 - Insert at the end");
+    printf("\n 3 - Insert before a node");
+    printf("\n 4 - Insert after a node");
+    printf("\n 5 - Delete at the begnning");
+    printf("\n 6 - Delete at the end");
+    printf("\n 7 - Delete in the middle");
+    printf("\n 8 - Display the doubly linked list");
+    printf("\nEnter anything else to exit"); 
+    	printf("\nEnter your choice :");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+            case 1:
+                insertBegin();
+                break;
+            case 2:
+                insertEnd();
+				break;
+            case 3:
+				printf("\nEnter number to inserted before : ");
+                scanf("%d", &data);
+                insertBefore(data);
+                break;
+            case 4:
+                printf("\nEnter number to inserted after : ");
+                scanf("%d", &data);
+                insertAfter(data);
+                break;
+            case 5:
+				deleteBegin();
+				break;
+			case 6:
+				deleteEnd();
+				break;
+            case 7:
+                printf("\nEnter number to delete: ");
+                scanf("%d", &data);
+                deleteMid(data);
+                break;
+            case 8:
+                display();
+                break;
+            
+            default:
+            	exit(0);
+        }
+    }
+    return 0;
+}
+
+
+
+
+
